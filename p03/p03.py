7a20b5d118f5d6d25a0412096a1c284e898b3ca5
kendea1221 <kenshirotamata@gmail.com>
2025-05-09 22:30:03 +0900

    feat: add p03

---
+++ p03/p03.py
@@ -0,0 +1,6 @@
+examL = ["前期中間","前期期末","後期中間","後期期末"]
+scoreL = ["25","50","75","100"]
+
+index = int(input("0~3の数値を入力してください:"))
+
+print(examL[index],"の成績は",scoreL[index],"です")
---
+++ p03/p03ex.py
@@ -0,0 +1,7 @@
+tempL = [0, 0, 0]
+
+tempL[0] = int(input("1つ目の数値:"))
+tempL[1] = int(input("2つ目の数値:"))
+tempL[2] = int(input("3つ目の数値:"))
+
+print(tempL)
